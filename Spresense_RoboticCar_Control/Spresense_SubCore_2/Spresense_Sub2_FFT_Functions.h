ef58f7ba5091f00efb440af2fe4125ed09cb7c34
max12345 <max98tkc@gmail.com>
2022-10-19 12:18:52 +0900

    20221019

---
+++ Spresense_RoboticCar_Control/Spresense_SubCore_2/Spresense_Sub2_FFT_Functions.h
@@ -0,0 +1,43 @@
+// Written by MAX (Created 18-10-2022)
+
+// Features : FFT related functions for Sub2
+
+//--------------------------------------------------------//
+
+#ifndef SPRESENSE_SUB2_FFT_FUNC
+#define SPRESENSE_SUB2_FFT_FUNC
+
+/*  Estimate inner/outer layer
+    
+    return value:
+    0 = inside
+    1 = x-axis outside
+    2 = y-axis outside
+    3 = xy-axis outside (corner)*/
+uint8_t inner_outer_estimate();
+
+// helper function for displaying variable value
+void fft_data_print_out();
+
+// helper function for updating phi
+void phi_update(uint32_t msgdata);
+
+// helper function for updating dx_phi
+void dx_phi_update(uint32_t msgdata);
+
+// helper function for updating dy_phi
+void dy_phi_update(uint32_t msgdata);
+
+// init all phi arrays
+void phi_init();
+
+// init FFT_calculation class
+void arm_fft_init();
+
+// (from Origane san) get peak index & calc frequency
+float get_peak_frequency(float* FFT_result_inScope);
+
+// (from Origane san) calc swarm size
+float length_estimation(float f_peak_inScope);
+
+#endif
--- Spresense_RoboticCar_Control/Spresense_SubCore_2/Spresense_SubCore_2.ino
+++ Spresense_RoboticCar_Control/Spresense_SubCore_2/Spresense_SubCore_2.ino
@@ -18,47 +18,13 @@
 //--------------------------------------------------------//
 
 // Functions Declaration
-float get_peak_frequency(float* FFT_result_inScope);
-float length_estimation(float f_peak_inScope);
-bool fft_init();
-
-// float* cal_power_spectrum(float* FFT_result_inScope);   // really need(?)
-uint8_t inner_outer();
 
 //--------------------------------------------------------//
 
-arm_rfft_fast_instance_f32 FFT_instance;
-float       phi_buffer[FFT_LEN];
-float       input_buffer[FFT_LEN];
-float       output_buffer[FFT_LEN];
-float       FFT_result[FFT_LEN];
-float       power_spectrum[FFT_LEN/2];
-uint16_t    FFT_countdown_sub2 = FFT_LEN;
-float       f_peak;
-float       length_estimate;
-
-float       dx_phi_buffer[FFT_LEN];
-float       dx_input_buffer[FFT_LEN];
-float       dx_output_buffer[FFT_LEN];
-float       dx_FFT_result[FFT_LEN];
-float       dx_power_spectrum[FFT_LEN/2];
-
-float       dy_phi_buffer[FFT_LEN];
-float       dy_input_buffer[FFT_LEN];
-float       dy_output_buffer[FFT_LEN];
-float       dy_FFT_result[FFT_LEN];
-float       dy_power_spectrum[FFT_LEN/2];
-
 void setup(void){
     MP.begin();
-    if (!fft_init())
-    {
-        MPLog("Error : FFT_LEN invalid !! Please check definition of fft_init().");
-    }
-    for (int i = 0; i < FFT_LEN; i++)
-    {
-        phi_buffer[i] = 0.0;
-    }
+    arm_fft_init();
+    phi_init();    
 }
 void loop(void){
     int8_t msgid;
@@ -70,258 +36,64 @@ void loop(void){
     {
     case C2_T0_FFT:{
         // get phi
-        float temp_phi = ((float)msgdata-FFT_MIDSHIFT)/FFT_MSG_SCALE;
-        for (int i = FFT_LEN-1; i > 0; i--)
-        {
-            phi_buffer[i] = phi_buffer[i-1];
-        }
-        phi_buffer[0] = temp_phi;
-        for (int i = 0; i < FFT_LEN; i++)
-        {
-            input_buffer[i] = phi_buffer[i];
-        }
+        phi_update(msgdata);
 
         // get dx_phi
-        // float temp_dx_phi;
-        // MP.Recv(&msgid, &msgdata);
-        // if (msgid == C2_T2_DXPHI)
-        // {
-        //     temp_dx_phi = ((float)msgdata-FFT_MIDSHIFT)/FFT_MSG_SCALE;
-        // }
-        // for (int i = FFT_LEN-1; i > 0; i--)
-        // {
-        //     dx_phi_buffer[i] = dx_phi_buffer[i-1];
-        // }
-        // dx_phi_buffer[0] = temp_dx_phi;
-        // for (int i = 0; i < FFT_LEN; i++)
-        // {
-        //     dx_input_buffer[i] = dx_phi_buffer[i];
-        // }
+        MP.Recv(&msgid, &msgdata);
+        if (msgid != C2_T1_DXPHI){break;}
+        dx_phi_update(msgdata);
         
         //get dy_phi
-        float temp_dy_phi;
         MP.Recv(&msgid, &msgdata);
-        if (msgid == C2_T3_DYPHI)
-        {
-            temp_dy_phi = ((float)msgdata-FFT_MIDSHIFT)/FFT_MSG_SCALE;
-        }
-        for (int i = FFT_LEN-1; i > 0; i--)
-        {
-            dy_phi_buffer[i] = dy_phi_buffer[i-1];
-        }
-        dy_phi_buffer[0] = temp_dy_phi;
-        for (int i = 0; i < FFT_LEN; i++)
-        {
-            dy_input_buffer[i] = dy_phi_buffer[i];
-        }
+        if (msgid != C2_T2_DYPHI){break;}
+        dy_phi_update(msgdata);
         
         // FFT calculation
+        static uint16_t FFT_countdown_sub2 = FFT_LEN;
         if (FFT_countdown_sub2 == 0)
         {
-            FFT_countdown_sub2 = 8;
+            // reset count
+            FFT_countdown_sub2 = FFT_CALC_RATE;
+            
+            // FFT & Power Spectrum
+            // ->-> rfft->FFT, cmplx_mag->complex magnitude
+            // -> phi
             arm_rfft_fast_f32(&FFT_instance, input_buffer, output_buffer, 0);
             arm_cmplx_mag_f32(output_buffer, FFT_result, FFT_LEN/2);
-            // arm_rfft_fast_f32(&FFT_instance, dx_input_buffer, dx_output_buffer, 0);
-            // arm_cmplx_mag_f32(dx_output_buffer, dx_FFT_result, FFT_LEN/2);
+            // -> dx_phi
+            arm_rfft_fast_f32(&FFT_instance, dx_input_buffer, dx_output_buffer, 0);
+            arm_cmplx_mag_f32(dx_output_buffer, dx_FFT_result, FFT_LEN/2);
+            // -> dy_phi
             arm_rfft_fast_f32(&FFT_instance, dy_input_buffer, dy_output_buffer, 0);
             arm_cmplx_mag_f32(dy_output_buffer, dy_FFT_result, FFT_LEN/2);
-            f_peak = get_peak_frequency(FFT_result);
+
+            // determine if need to self-excite by finding peak
+            float f_peak = get_peak_frequency(FFT_result);
             if (f_peak < 0) // 検出失敗
             {
-                MP.Send(C2_T1_NO_PEAK, C2_T1_NO_PEAK);  // メインコアに失敗を通報
+                MP.Send(C2_T3_NO_PEAK, C2_T3_NO_PEAK);  // メインコアに失敗を通報
             }
-            length_estimate = length_estimation(f_peak);
+            // determine swarm size (not used now)
+            // float length_estimate = length_estimation(f_peak);
             // MPLog("Length Estimate : %4.2f\n", length_estimate);
 
-            inner_outer();
+            // estimate inner/outer layer  ********
+            inner_outer_estimate();
 
-            static int temp = 0;
-            if (temp==10)
-            {
-                // MPLog("phi input buffer\n");
-                // for (int i = 0; i < FFT_LEN; i++)
-                // {
-                //     MPLog("%5.5f\n", input_buffer[i]);
-                // }
-
-                // MPLog("phi output buffer\n");
-                // for (int i = 0; i < FFT_LEN/2; i++)
-                // {
-                //     MPLog("%5.5f        %5.5f\n", output_buffer[2*i], output_buffer[2*i+1]);
-                // }
-
-                // MPLog("phi FFT result\n");
-                // for (int i = 0; i < FFT_LEN/2; i++)
-                // {
-                //     MPLog("%5.5f\n", FFT_result[i]);
-                // }
-
-                // MPLog("dy phi input buffer\n");
-                // for (int i = 0; i < FFT_LEN; i++)
-                // {
-                //     MPLog("%5.5f\n", dy_input_buffer[i]);
-                // }
-
-                // MPLog("dy phi output buffer\n");
-                // for (int i = 0; i < FFT_LEN/2; i++)
-                // {
-                //     MPLog("%5.5f        %5.5f\n", dy_output_buffer[2*i], dy_output_buffer[2*i+1]);
-                // }
-
-                // MPLog("dy phi FFT result\n");
-                // for (int i = 0; i < FFT_LEN/2; i++)
-                // {
-                //     MPLog("%5.5f\n", dy_FFT_result[i]);
-                // }
-            }
-            temp++;
+            // print out data to Serial Monitor
+            fft_data_print_out();
 
         } else {
             FFT_countdown_sub2--;
         }
         break;
     }
-    case C2_T1_NO_PEAK:{
-        // Just placeholder, C2_T1_NO_PEAK is for sending back to Main core
-        break;
-    }
-    // case C2_T2_DXPHI:    // not for here
-    // case C2_T3_DYPHI:    // not for here
+    // case C2_T1_DXPHI:    // not for receiving
+    // case C2_T2_DYPHI:    // not for receiving
+    // case C2_T3_NO_PEAK:  // not for receiving
     default:
         break;
     }
-}
-
-float get_peak_frequency(float* FFT_result_inScope){
-    float Ts = 0.14f;
-    float g_fs = 1.0f/Ts;
-    uint32_t index = 0;
-    float maxValue;
-    float delta;
-    float peakFs;
-
-    for (int i = 1; i < FFT_LEN/2; i++){ // ピーク候補から0Hzは除外
-        if( (FFT_result_inScope[i] > FFT_result_inScope[i-1]) && (FFT_result_inScope[i+1] < FFT_result_inScope[i]) ){ // 極大値
-            if(FFT_result_inScope[i] > PEAK_POWER_THRESHOLD){ // 閾値以上なら有効
-                index = i;
-                break;  // break if found
-            }
-        }
-    }
-    if (index == 0)  // ピーク検出失敗時，-1を返す
-    {
-        return -1;
-    }
-    
-    delta = 0.5 * (FFT_result_inScope[index-1] - FFT_result_inScope[index+1])
-            / (FFT_result_inScope[index-1] + FFT_result_inScope[index+1] - (2.0f * FFT_result_inScope[index]));
-    peakFs = (index + delta) * g_fs / (FFT_LEN-1);
-
-    // MPLog("index : %d\n", index);
-    // MPLog("peakFs : %4.2f\n", peakFs);
-
-    return peakFs;
-}
-
-float length_estimation(float f_peak_inScope){
-    if (f_peak_inScope<=0){
-        return -1;
-    }
-    return 0.5f*sqrt(kappa)/f_peak_inScope;  // l = sqrt(kappa)/(2*f_peak)
-}
-
-//probably no need
-// float* cal_power_spectrum(float* FFT_result_inScope){
-//     int half_FFT_LEN = FFT_LEN/2;
-//     for (int i = 0; i < half_FFT_LEN; i++)
-//     {
-//         power_spectrum[i] = FFT_result_inScope[i] * FFT_result_inScope[i] / half_FFT_LEN / half_FFT_LEN;
-//     }
-//     return power_spectrum;
-// }
-
-/* return value:
-   0 = inside
-   1 = x-axis outside
-   2 = y-axis outside
-   3 = xy-axis outside (corner)
-*/ //TODO dx direction
-uint8_t inner_outer(){
-    
-    int dx_peak_index = 0;
-    int dy_peak_index = 0;
-    int half_FFT_LEN = FFT_LEN/2;
-    
-    for (int i = 1; i < half_FFT_LEN; i++) // ピーク候補から0Hzは除外
-    {
-        if( (dy_FFT_result[i] > dy_FFT_result[i-1]) && (dy_FFT_result[i] > dy_FFT_result[i+1]) )
-        {
-            if (dy_FFT_result[i] > PEAK_POWER_THRESHOLD)
-            {
-                dy_peak_index = i;
-                MPLog("%5.5f        %5.5f\n", FFT_result[i], dy_FFT_result[i]);
-                // MPLog("\n");
-                break;
-            }
-            
-        }
-    }
-
-    if (FFT_result[dy_peak_index] < dy_FFT_result[dy_peak_index])
-    {
-        digitalWrite(OUTSIDE_LED, LOW);
-        digitalWrite(INSIDE_LED, HIGH);
-    } else {
-        digitalWrite(OUTSIDE_LED, HIGH);
-        digitalWrite(INSIDE_LED, LOW);
-    }
-    
-    
-
-    // // assume peak exist
-    // // no threshold requirement
-    // for (int i = 1; i < FFT_LEN/2; i++){ // ピーク候補から0Hzは除外
-    //     if( (FFT_result_inScope[i] > FFT_result_inScope[i-1]) && (FFT_result_inScope[i+1] < FFT_result_inScope[i]) ){ // 極大値
-    //         index = i;
-    //         break;  // break if found
-    //     }
-    // }
-
-    return 0;
-}
-
-bool fft_init(){
-     switch (FFT_LEN){
-       case 32:
-         arm_rfft_32_fast_init_f32(&FFT_instance);
-         break;
-       case 64:
-         arm_rfft_64_fast_init_f32(&FFT_instance);
-         break;
-       case 128:
-         arm_rfft_128_fast_init_f32(&FFT_instance);
-         break;
-       case 256:
-         arm_rfft_256_fast_init_f32(&FFT_instance);
-         break;
-       case 512:
-         arm_rfft_512_fast_init_f32(&FFT_instance);
-         break;
-       case 1024:
-         arm_rfft_1024_fast_init_f32(&FFT_instance);
-         break;
-       case 2048:
-         arm_rfft_2048_fast_init_f32(&FFT_instance);
-         break;
-       case 4096:
-         arm_rfft_4096_fast_init_f32(&FFT_instance);
-         break;
-       default:
-         puts("error!");
-         return false;
-         break;
-     }
-     return true;
 }
 
 #endif
