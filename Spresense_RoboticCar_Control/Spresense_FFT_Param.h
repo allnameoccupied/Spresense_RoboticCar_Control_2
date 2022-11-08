3afcdd9514f8bf55bac89d1efc9dff03cf7cb1a1
max12345 <max98tkc@yahoo.com.hk>
2022-11-08 10:09:48 +0900

    20221108

--- Spresense_RoboticCar_Control/Spresense_FFT_Param.h
+++ Spresense_RoboticCar_Control/Spresense_FFT_Param.h
@@ -37,22 +37,13 @@ arm_rfft_fast_instance_f32  FFT_instance;
 
 // FFT Parameter
 #define dt                  FFT_TIME_RESOLUTION * FFT_UPDATE_PERIOD_US
-// #define Omega_0             20.0            // 固有角速度 [rad\s]（振動）   // original
 #define Omega_0             20.0            // 固有角速度 [rad\s]（振動）
-// #define kappa               0.625             // 結合強度（振動型）    //original
 #define kappa               0.5             // 結合強度（振動型）
 #define my_gamma            0.20            // 粘性係数
 #define PEAK_POWER_THRESHOLD        3.0     // 観測している位相のピークパワーがこれを下回ったら検出扱いにしない
-#define SELF_EXITATION_INTENSITY    2.0     // 自励入力の大きさ
-#define GAMMA               0.09            // 動的粘性
-// #define GAMMA_CONST_1       0.09            // 動的粘性の定数項      //original
-// #define GAMMA_CONST_2       0.09            // 動的粘性のエッジ数に比例する項    //original
-#define GAMMA_CONST_1       GAMMA            // 動的粘性の定数項
-#define GAMMA_CONST_2       GAMMA            // 動的粘性のエッジ数に比例する項
-// #define DPHI_MULTIPLIER     0.65    //original
-#define DPHI_MULTIPLIER     1.0
-#define DXPHI_MULTIPLIER    DPHI_MULTIPLIER
-#define DYPHI_MULTIPLIER    DPHI_MULTIPLIER
+// #define SELF_EXITATION_INTENSITY    2.0     // 自励入力の大きさ
+#define GAMMA_CONST_1       0.09            // 動的粘性の定数項
+#define GAMMA_CONST_2       0.09            // 動的粘性のエッジ数に比例する項
 
 // FFT Variables
 double  mod_varphi          = 0.0;
@@ -68,29 +59,29 @@ double  c_fft               = 0.0;
 // FFT Runtime Variables
 bool    is_Self_Excitation = false;
 uint8_t PikaPika_light_sensor_life[PIKAPIKA_SENSOR_COUNT] = {0,0,0,0,0,0,0,0};
-#if (SUBCORE == 2)
-// -> only for FFT Core
-// ->-> phi
-float   phi_buffer[FFT_LEN];
-float   input_buffer[FFT_LEN];
-float   output_buffer[FFT_LEN];
-float   FFT_result[FFT_LEN];
-float   FFT_result_processed[FFT_LEN/8];
-uint8_t FFT_peak_trough_pos[FFT_LEN/8];
-// ->-> dx phi
-float   dx_phi_buffer[FFT_LEN];
-float   dx_input_buffer[FFT_LEN];
-float   dx_output_buffer[FFT_LEN];
-float   dx_FFT_result[FFT_LEN];
-float   dx_FFT_result_processed[FFT_LEN/8];
-uint8_t dx_FFT_peak_trough_pos[FFT_LEN/8];
-// ->-> dy phi
-float   dy_phi_buffer[FFT_LEN];
-float   dy_input_buffer[FFT_LEN];
-float   dy_output_buffer[FFT_LEN];
-float   dy_FFT_result[FFT_LEN];
-float   dy_FFT_result_processed[FFT_LEN/8];
-uint8_t dy_FFT_peak_trough_pos[FFT_LEN/8];
+uint8_t Judgement;
+#if (SUBCORE == 2)  // -> only for FFT Core
+    // ->-> phi
+    float   phi_buffer[FFT_LEN];
+    float   input_buffer[FFT_LEN];
+    float   output_buffer[FFT_LEN];
+    float   FFT_result[FFT_LEN];
+    float   FFT_result_processed[FFT_LEN/8];
+    uint8_t FFT_peak_trough_pos[FFT_LEN/8];
+    // ->-> dx phi
+    float   dx_phi_buffer[FFT_LEN];
+    float   dx_input_buffer[FFT_LEN];
+    float   dx_output_buffer[FFT_LEN];
+    float   dx_FFT_result[FFT_LEN];
+    float   dx_FFT_result_processed[FFT_LEN/8];
+    uint8_t dx_FFT_peak_trough_pos[FFT_LEN/8];
+    // ->-> dy phi
+    float   dy_phi_buffer[FFT_LEN];
+    float   dy_input_buffer[FFT_LEN];
+    float   dy_output_buffer[FFT_LEN];
+    float   dy_FFT_result[FFT_LEN];
+    float   dy_FFT_result_processed[FFT_LEN/8];
+    uint8_t dy_FFT_peak_trough_pos[FFT_LEN/8];
 #endif
 
 #endif
